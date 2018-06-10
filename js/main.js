1527fa8e54789ac2c339901993c479456c45aec2
Beka Tsalani <beqacalani@gmail.com>
2018-06-10 14:23:51 +0400

    finalize lightbox

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $().ready(function () {
 
+    var gallery = Array.prototype.slice.call($(".categories .show button.lightbox"));
+    var lightboxCounter = 0;
+
     $(".navigation li").click(function () {
         var index = $(this).index();
         var isactive = $(this).hasClass('active');
@@ -11,6 +14,7 @@ $().ready(function () {
             setTimeout(function () {
                 $(".categories > div").removeClass('show animated fadeIn');
                 $(".categories > div").eq(index).addClass('show animated fadeIn');
+                gallery = Array.prototype.slice.call($(".categories .show button.lightbox"));
             }, 500);
         }
     });
@@ -30,9 +34,36 @@ $().ready(function () {
     $("button.lightbox").click(function (event) {
         event.stopPropagation();
         event.stopImmediatePropagation();
+
+        var index = gallery.indexOf(this);
+        lightboxCounter = index;
+        applyGalleryItem(lightboxCounter);
         $(".lightbox").addClass('open');
         $(".site").addClass('close');
     });
+
+    $("div.lightbox button.prev").click(function (event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        if(lightboxCounter == 0){
+            lightboxCounter = gallery.length-1;
+        }else{
+            lightboxCounter--;
+        }
+        applyGalleryItem(lightboxCounter);
+    });
+
+    $("div.lightbox button.next").click(function (event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        if(lightboxCounter == (gallery.length-1)){
+            lightboxCounter = 0;
+        }else{
+            lightboxCounter++;
+        }
+        applyGalleryItem(lightboxCounter);
+    });
+
 
     $("button.close").click(function (event) {
         event.stopPropagation();
@@ -42,5 +73,16 @@ $().ready(function () {
     });
 
     $(".categories article").height($("main").height() / 2 - 20);
+
+    function applyGalleryItem(counter) {
+        var btn = gallery[counter];
+        var url = JSON.parse(btn.getAttribute('data-url'));
+        var image = btn.getAttribute('data-image');
+        var title = btn.getAttribute('data-title');
+        $("div.lightbox header h3").html(title);
+        $("div.lightbox header a").attr('href',url.url);
+        $("div.lightbox header a").html(url.name);
+        $("div.lightbox .gallerywrapper").html("<img src='"+image+"' alt='gallery img'>");
+    }
 
 });
